72fdec22fce716ec5d660e81a4ea560d9c06ad4e
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2023-01-18 00:36:48 +0500

    feat: добавил базовый защищённый роут

---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { Redirect, Route, RouteProps } from "react-router-dom";
+import getUserProfile from "../../services/utils/api/get-user-profile";
+import LoadingIcon from "../loading-icon/loading-icon";
+
+interface TProtectedProps extends RouteProps {
+  auth: string;
+  path: string;
+}
+
+export default function ProtectedRoute ({path, auth, children, ...props}: TProtectedProps) {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [role, setRole] = useState<string>('');
+
+  useEffect(() => {
+    setLoading(true);
+    getUserProfile().then((res) => {
+      setRole(res.role);
+      setLoading(false);
+    });
+  }, [])
+  
+  if (loading) {
+    return (
+      <main>
+        <LoadingIcon />
+      </main>
+    )
+  }
+
+  return (
+    <Route path={path} {...props}>
+      {auth === role ? children : <Redirect to='/missing' />}
+    </Route>
+  )
+}
