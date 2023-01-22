bc01adc4fff2008beacb30d79cc2c3b4e0f7f5cf
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2023-01-22 20:28:46 +0300

    Add button to reset filter

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,12 +1,34 @@
 import { useLocation } from 'react-router-dom';
+import styles from './filter.module.css';
 import { getCommentsRequest } from '../../services/utils/api/admin-comments';
 import { getStudentsRequest } from '../../services/utils/api/admin-students';
-import styles from './filter.module.css';
+import clearPic from './../../images/clear.svg';
 
 
 // фильтр отправляет запрос, но приходит неизмененный ответ.
 export const Filter = ({ setFunc }) => {
   const location = useLocation();
+
+  // показывает кнопку сброса фильтра при вводе
+  const onChange = () => {
+    const button = document.querySelector(`.${styles.button}`);
+    button.classList.add(`${styles.visible}`)
+  }
+
+  // сбрасывает фильтр
+  const clearHangler = (evt) => {
+    evt.target.parentElement.querySelector('input').value = '';
+    if (location.pathname === '/students') {
+      getStudentsRequest({ search: '' })
+        .then(res => { res?.items.length ? setFunc(res.items) : setFunc([]) });
+    }
+    if (location.pathname === '/comments') {
+      getCommentsRequest({ search: '' })
+        .then(res => { setFunc(res.items) });
+    }
+  }
+
+  //отправить запрос с параметрами
   const onSubmit = (evt) => {
     evt.preventDefault();
     const searchCondition = evt.target.condition.value.toLowerCase();
@@ -15,10 +37,12 @@ export const Filter = ({ setFunc }) => {
         .then(res => { res?.items.length ? setFunc(res.items) : setFunc([]) });
     }
     if (location.pathname === '/comments') {
+      debugger
       getCommentsRequest({ search: searchCondition })
         .then(res => { setFunc(res.items) });
     }
   }
+
   return (
     <form className={styles.container} onSubmit={onSubmit}>
       <label htmlFor='filter_input' className={styles.name} >Фильтровать</label>
@@ -26,8 +50,15 @@ export const Filter = ({ setFunc }) => {
         id='filter_input'
         name='condition'
         className={styles.input}
-        placeholder='По имени или фамилии, или почте, или номеру когорты (введите любой из этих параметров)' >
+        placeholder='По имени или фамилии, или почте, или номеру когорты (введите любой из этих параметров)'
+        onChange={onChange} >
       </input>
+      <button
+        className={styles.button}
+        style={{ backgroundImage: `url(${clearPic})` }}
+        type='button'
+        onClick={(evt) => clearHangler(evt)} >
+      </button>
     </form>
   )
 }
